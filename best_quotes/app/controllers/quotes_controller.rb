a230f731564cd6f2cd6a6fcfb40e94b0bccbf7e8
Colin Frankish <colin.frankish@bcsg.com>
2015-01-19 18:08:45 +0000

    Chapter 2 debugged

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,6 +1,10 @@
 class QuotesController < Rulers::Controller
   def a_quote
-    "Hi there Mr App Thing"
+    "Hi there Mr App Thing" + " some other stuff" + "\n<pre>\n#{env}\n</pre>"
+  end
+
+  def exception
+    raise "It's a bad error kidz!!"
   end
 end
 
