42b0fbbb3d3b98ac8169519d733510e7956eaa90
Colin Frankish <colin.frankish@bcsg.com>
2015-02-11 13:37:43 +0000

    Building a json model... It doesn't quite work yet.... Check FileModel code..

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -8,6 +8,25 @@ class QuotesController < Rulers::Controller
     raise "It's a bad error kidz!!"
   end
 
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
+  def quote_1
+    quote_1 = Rulers::Model::FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web_user",
+      "quote" => "A picture is worth a thousand faces",
+      "attribution" => "Me"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
 
   #So this comment should restart the server, right ? Due to the rerun gem....
 end
